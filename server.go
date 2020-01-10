4f503cb2692659d6ae6d879a366bc048cfc33662
Qu Zhe <zqu@thoughtworks.com>
2020-01-11 03:40:39 +0800

    chore: add revive as lint tool

--- server.go
+++ server.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintln(w, "Hello World!")
 }
 
